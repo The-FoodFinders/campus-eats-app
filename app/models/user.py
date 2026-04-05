0ecf11377c1965a5ca0bca366a6b642af28ba03c
emilymohammed24 <157489330+emilymohammed24@users.noreply.github.com>
2026-04-04 22:18:08 -0400

    Update user.py

--- app/models/user.py
+++ app/models/user.py
@@ -3,13 +3,15 @@ from typing import Optional, List
 from pydantic import EmailStr
 
 
-class UserBase(SQLModel,):
+class UserBase(SQLModel):
     username: str = Field(index=True, unique=True)
     email: EmailStr = Field(index=True, unique=True)
     password: str
-    role:str
+    role: str
+
 
 class User(UserBase, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
 
     orders: List["Order"] = Relationship(back_populates="user")
+    reviews: List["Review"] = Relationship(back_populates="user")
