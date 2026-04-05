02d51e2ac3985bc850739a088d4426631787fe0d
emilymohammed24 <157489330+emilymohammed24@users.noreply.github.com>
2026-04-04 22:19:25 -0400

    Update __init__.py

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,3 +3,4 @@ from app.models.restaurant import Restaurant
 from app.models.order import Order
 from app.models.order_item import OrderItem
 from app.models.menu import MenuItem
+from app.models.review import Review
